e877766d121c3e1a2fdcda24c90822271c7251dc
David <32504470+dleweyiv@users.noreply.github.com>
2017-10-04 22:59:32 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -20,15 +20,44 @@ class TourGuideSkill(MycroftSkill):
 						require("FunFactVillanovaKeyword").build()
 				self.register_intent(fun_fact_villanova_intent, self.handle_fun_fact_villanova_intent)
 
+				# ---------------------------------------------------------------------------------
+
 				college_majors_intent = IntentBuilder("CollegeMajorsIntent"). \
 						require("CollegeMajorsKeyword").build()
 				self.register_intent(college_majors_intent, self.handle_college_majors_intent)
+
+				# ---------------------------------------------------------------------------------
+
+				college_minors_intent = IntentBuilder("CollegeMinorsIntent"). \
+						require("CollegeMinorsKeyword").build()
+				self.register_intent(college_minors_intent, self.handle_college_minors_intent)
+
+				# ---------------------------------------------------------------------------------
+
+				engineering_resources_intent = IntentBuilder("EngineeringResourcesIntent"). \
+						require("EngineeringResourcesKeyword").build()
+				self.register_intent(engineering_resources_intent, self.handle_engineering_resources_intent)
+
+				# ---------------------------------------------------------------------------------
+
+				engineering_projects_trips_intent = IntentBuilder("EngineeringProjectsTripsIntent"). \
+						require("EngineeringProjectsTripsKeyword").build()
+				self.register_intent(engineering_projects_trips_intent, self.handle_engineering_projects_trips_intent)
 
 
 		def handle_fun_fact_villanova_intent(self, message):
 				self.speak_dialog("fun.fact.villanova")
 
 		def handle_college_majors_intent(self, message):
+				self.speak_dialog("college.majors")
+
+		def handle_college_minors_intent(self, message):
+				self.speak_dialog("college.minors")
+
+		def handle_engineering_resources_intent(self, message):
+				self.speak_dialog("college.majors")
+
+		def handle_engineering_projects_trips_intent(self, message):
 				self.speak_dialog("college.majors")
 
 		def stop(self):
